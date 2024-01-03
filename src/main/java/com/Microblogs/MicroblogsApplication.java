e702b7f6532b9ac3cb3cc69de78b3c1440cd627f
alojine <benaspaliokas@gmail.com>
2024-01-03 10:22:36 +0200

    init

---
+++ src/main/java/com/Microblogs/MicroblogsApplication.java
@@ -0,0 +1,13 @@
+package com.Microblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroblogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroblogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Microblogs/MicroblogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microblogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroblogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
